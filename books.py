19bfd4b69a092aae0926cccd883f776fd9ac279e
Max-Frisch <max-frisch@gmx.net>
2023-11-25 00:14:50 +0700

    added post, update and delete endpoints

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -20,8 +20,48 @@ async def read_all_books():
 async def read_all_books():
     return {'book title': 'My favorite book!'}
 
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+        
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+        
+@app.delete("/books/delete_book")
+async def delete_book(book_to_delete=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_to_delete.get('title').casefold():
+            BOOKS.pop(i)
